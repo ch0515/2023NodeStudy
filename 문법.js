7c4ef3123247792998cdae89723f211b07dc0f2a
chaemin <w2114@e-mirim.hs.kr>
2023-03-15 09:54:35 +0900

    기본 사용법

--- 문법.js
+++ 문법.js
@@ -3,6 +3,24 @@
  null, NaN, 0, 빈 문자열("",'',``), undefined, flase
 */
 
-const a = 30;
-console.log(a); // 30
-//a = 10; //에러 const는 값을 바꿀 수 없음
+// const a = 30;
+// console.log(a); // 30
+// //a = 10; //에러 const는 값을 바꿀 수 없음
+
+//함수
+function add(n1, n2){
+  return n1+n2;
+}
+
+result = add(1,2);
+console.log(result); //3
+
+plus = add; //plus도 add()의 역할을 할 수 있음
+result = plus(3,4);
+console.log(result);
+
+sub = function(n1,n2){
+  return n1-n2;
+}
+result = sub(3,2);
+console.log(result);
