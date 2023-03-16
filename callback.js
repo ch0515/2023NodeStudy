2aeb31a75676f602c7904f3ce6e322e8134b3e3e
chaemin <w2114@e-mirim.hs.kr>
2023-03-16 15:05:18 +0900

    callback2 익명함수 사용

--- callback.js
+++ callback.js
@@ -1,13 +1,11 @@
 function somefunc(callback){
   console.log("somefunc 실행");
 
-  //somefunc의 기능을 수ㅐㅎㅇ하고 난 후에
+  //somefunc의 기능을 수행하고 난 후에
   //callback()가 실행된다.
   callback();
 }
 
-function cb(){
+somefunc(function(){
   console.log("콜백함수 실행");
-}
-
-somefunc(cb);
+});
