c6b3787af4abd5b55db2dbe37372e27a9404b45d
chaemin <w2114@e-mirim.hs.kr>
2023-03-16 14:05:20 +0900

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+const cathy = {
+  "name" : "cathy",
+  "age" : 3,
+  "skills" : ["자바스크립트", "인공지능", "자바", "리엑트", "코볼"]
+};
+
+console.log(cathy);
+console.log(cathy.name);
+cathy["name"] = "만복이";
+console.log(cathy["name"]);
+
+cathy["성별"] = "femal";
+console.log(cathy.성별);
