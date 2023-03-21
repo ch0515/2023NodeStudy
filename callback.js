8a8aa411c151437a67aec3659c87983cb4230503
chaemin <w2114@e-mirim.hs.kr>
2023-03-22 08:52:35 +0900

    콜백

--- callback.js
+++ callback.js
@@ -2,7 +2,7 @@ function somefunc(callback){
   console.log("somefunc 실행");
 
   //somefunc의 기능을 수행하고 난 후에
-  //callback()가 실행된다.
+  //callback()가 실행된다.  자바스크립트에서 순서가 잇어야 하는데 그거때문에 쓴대 비동기 왜 쓰는지
   callback();
 }
 
