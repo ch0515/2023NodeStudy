becc1430982b4d7d57fdca67f8b4eff14ccadd06
chaemin <w2114@e-mirim.hs.kr>
2023-03-22 09:05:38 +0900

    ==, ===

---
+++ 동등비교.js
@@ -0,0 +1,11 @@
+//동등연산자(abstract eauility)
+console.log(1 == "1");
+
+// 엄격연산자(strict equility)
+console.log(1 === "1"); // 비교할때 세개 쓰는 거 권장
+
+console.log("hello" == "hello");
+console.log("hello" === "hello");
+
+console.log(0 == flase); //true
+console.log(0 === flase); //flase
