a21b227d086cdac045d2a1da86d761175afd7c1d
chaemin <w2114@e-mirim.hs.kr>
2023-03-22 09:48:05 +0900

    null undefined

---
+++ Ex0909_1.js
@@ -0,0 +1,11 @@
+let foo;
+console.log(foo);
+
+let obj = {};
+console.log(obj.prop);
+
+let bar = null;
+console.log(bar);
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
