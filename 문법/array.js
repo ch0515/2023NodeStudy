d1f04937b4ebd0161288b3839ae777f425c58d7d
chaemin <w2114@e-mirim.hs.kr>
2023-04-12 10:02:17 +0900

    array.findindex()

--- 문법/array.js
+++ 문법/array.js
@@ -2,5 +2,12 @@ const arr = ["apple", "banana", "cherry", "apple"];
 
 // data : 데티터, index : 데이터에 해당하는 idx()
 arr.forEach(function (data, index) {
-  console.log(`${index} : ${data}`);
+  //console.log(`${index} : ${data}`);
 });
+
+//return 값이 true에 해당하는 idx반환, 없으면 -1 반환
+const cherryIndex = arr.findIndex(function (data) {
+  return data === "cherry";
+});
+
+console.log(cherryIndex);
