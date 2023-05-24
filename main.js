958379cdc143a3ecd0bcd0e3b476eb18a88f66b2
chaemin <w2114@e-mirim.hs.kr>
2023-05-24 09:56:42 +0900

    express 실행하기

--- main.js
+++ main.js
@@ -1,45 +1,9 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require('express');
 
-const app = http.createServer(function (req, res) {
-  let _url = req.url; // 문자열 형태
-  //문자열 형태의 _url을 object 형태로 변환
-  let queryData = url.parse(_url, true).query;
-  let pathname = url.parse(_url, true).pathname;
+const app = express();
 
-  if (pathname === "/") {
-    if (queryData == undefined) {
-      res.writeHead(200);
-      res.end("undefined");
-    } else {
-      //description : readFile을 통해 읽어온 실제 내용
-      fs.readFile(`data/${qudryData.id}`, "utf-8", function (err, description) {
-        const title = queryData.id;
-        const template = `
-        <!DOCTYPE html>
-        <html lang="ko">
-          <head>
-            <title>WEB1 - ${title}</title>
-          </head>
-          <body>
-            <h1>WEB</h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
-        res.writeHead(200);
-        req.end(template);
-      });
-    }
-  }
-  res.end(queryData.id);
+app.get('/', function(req, res, next){
+  res.send('Hello World');
 });
 
-app.listen(3334);
+app.listen(3000);
